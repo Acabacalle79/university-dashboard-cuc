dbe593271862fa1502a004c7899d95a7447e6739
Pedro <160541421+Acabacalle79@users.noreply.github.com>
2025-11-23 19:45:06 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,9 +3,13 @@ import pandas as pd
 import numpy as np
 import os
 import pickle
+
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 MODEL_PATH = "modelo_iris.pkl"
 
